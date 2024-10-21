56227ff66cbcd10dbe674835f5239c2d7288f1b9
Zeez6 <9864@holbertonstudents.com>
2024-10-21 17:04:55 +0200

    quest1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,20 +6,20 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (last digit of > 5)
-	{
-	printf("%10 is greather than 5\n" n);
-	}
-	else if (last digit of == 0)
-	{
-	printf("%10 and is 0\n" n);
-	}
-	else
-	{
-	printf("%10 and is less than 6 and not 0\n" n);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (last digit of > 5)
+{
+printf("%10 is greather than 5\n" n);
 }
-	return (0);
+else if (last digit of == 0)
+{
+printf("%10 and is 0\n" n);
+}
+else
+{
+printf("%10 and is less than 6 and not 0\n" n);
+}
+return (0);
 }
