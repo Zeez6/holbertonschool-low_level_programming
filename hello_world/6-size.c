73c45a4d60759e97540a9c3f28bc9fb59383c198
root <root@Zeez>
2024-10-21 12:10:22 +0200

    quest6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main (void) {
 // Use %zu for size_t if your compiler supports it.
-printf("sizeof(int)       = %d\n", sizeof(int));
-printf("sizeof(float)     = %d\n", sizeof(float));
-printf("sizeof(double)    = %d\n", sizeof(double));
-printf("sizeof(char)      = %d\n", sizeof(char));
-printf("sizeof(167)       = %d\n", sizeof(167));
-printf("sizeof(3.1415926) = %d\n", sizeof(3.1415926));
-printf("sizeof('$')       = %d\n", sizeof('$'));
+printf("sizeof(int)       = %d\n", sizeof(4));
+printf("sizeof(float)     = %d\n", sizeof(4));
+printf("sizeof(double)    = %d\n", sizeof(8));
+printf("sizeof(char)      = %d\n", sizeof(1));
+printf("sizeof(167)       = %d\n", sizeof(4));
+printf("sizeof(3.1415926) = %d\n", sizeof(8));
+printf("sizeof('$')       = %d\n", sizeof(4));
 return (0);
 }
