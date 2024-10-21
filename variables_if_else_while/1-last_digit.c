7b071dcbd7a05030985ebb54fb71484e64fbb925
Zeez6 <9864@holbertonstudents.com>
2024-10-21 16:59:54 +0200

    quest1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;	
+	if (last > 5)
+	{
+	printf("%10 is greather than 5\n" n);
+	}
+	else if (last == 0)
+	{
+	printf("%10 and is 0\n" n);
+	}
+	else
+	{ 
+	printf("%10 and is less than 6 and not 0\n" n);
+}
+	return (0);
+}
