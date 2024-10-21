d9455ed1bbe4002a99318280b9a3f30764c99b38
root <root@Zeez>
2024-10-21 12:33:04 +0200

    quest6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-
 /**
-* main - Entry point
+* main - Entry point of the program
+*
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-Printf("Size of a char: %lu byte(s)\n", sizeof(char));
-Printf("Size of an int: %lu byte(s)\n", sizeof(int));
-Printf("Size of long int: %lu byte-s)\n", sizeof(long int));
-Printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-Printf("Size of float: %lu byte(s)\n", sizeof(float));
-
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
