0829b3f4d399a52db7824b113d89e1c20b542c3e
Zeez6 <9864@holbertonstudents.com>
2024-10-22 11:39:46 +0200

    quest3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
-* main - Prints the alphabet in lowercase.
+* main - Prints the alphabet in lowercase and uppercase
 *
 * Return: Always 0.
 */
 int main(void)
 {
 char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-while  (letter = 'A'; letter <= 'Z'; letter++)
+for (letter = 'a' ; letter <= 'z' ; letter++)
+while  (letter = 'A' ; letter <= 'Z' ; letter++)
 putchar(letter);
 putchar('\n');
 return (0);
