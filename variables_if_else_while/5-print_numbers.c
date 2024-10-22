5b7f829cff5ade9eff783474f5c7e79e54b3c2ca
Zeez6 <9864@holbertonstudents.com>
2024-10-22 15:28:04 +0200

    quest5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-
 /**
-* main - Entry point
+* main - Prints all single digit numbers of base 10 starting from 0.
 *
-* Description: Prints a specific message to stdout using puts.
-* Return: Always 0 (Success)
+* Return: Always 0.
 */
 int main(void)
 {
-int 0;
-srand(time(0));
-if (0 >= 0; 0 < 10; 0++);
-putchar(0);
-putchar('\n');
+int num;
+for (num = 0; num < 10; num++)
+printf("%d", num);
+printf("\n");
 return (0);
 }
