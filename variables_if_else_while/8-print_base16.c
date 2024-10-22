1e64d8d3967bf3539407adffdf3663f13b3612fe
Zeez6 <9864@holbertonstudents.com>
2024-10-22 16:50:23 +0200

    quest8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int main(void)
 int num;
 char letter;
 for (num = 0; num < 10; num++)
-putchar((num % 10) + '0');
+putchar((num % 16) + '0');
 for (letter = 'a'; letter <= 'z'; letter++)
 putchar(letter);
 putchar('\n');
