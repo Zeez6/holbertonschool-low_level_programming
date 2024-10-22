c67684aa8a93a8f17ba5a79488c14ab364573cd6
Zeez6 <9864@holbertonstudents.com>
2024-10-22 14:57:30 +0200

    quest5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Prints a specific message to stdout using puts.
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int 0;
+srand(time(0));
+if (0 >= 0; 0 < 10; 0++);
+putchar(0);
+putchar('\n');
+return (0);
+}
