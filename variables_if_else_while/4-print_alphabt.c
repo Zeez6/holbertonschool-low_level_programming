0f477a5e2761d4f753c9aa1ed2001da867a06673
Zeez6 <9864@holbertonstudents.com>
2024-10-22 14:25:41 +0200

    quest4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lowercase and uppercase
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+if (letter != 'q' && letter != 'e'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
