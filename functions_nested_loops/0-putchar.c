bf22f8521bb0801bed8affb42c89db75daef1f1a
Zeez6 <9864@holbertonstudents.com>
2024-10-23 10:54:26 +0200

    quest0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
 * main - Prints all single digit numbers of base 10 starting from 0.
 *
 * Return: Always 0.
 */
-
-int main(void)
-int _putchar(char c)
+int main(void);
 {
-putchar("%d_putchar\n");
+int write(int filedes, const char *buf, unsigned int nbytes);
+write("1, _putchar\n", 8);
 return (0);
 }
