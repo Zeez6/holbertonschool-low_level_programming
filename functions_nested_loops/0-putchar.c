b6c7294c2d8b978ccb3bf4055841bd7f4689132a
Zeez6 <9864@holbertonstudents.com>
2024-10-23 09:22:29 +0200

    quest0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Prints all single digit numbers of base 10 starting from 0.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
+
