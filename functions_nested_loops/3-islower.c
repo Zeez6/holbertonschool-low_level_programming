1bdc337ca710dd977f7a761f6f52e4d2a5779aa2
Zeez6 <9864@holbertonstudents.com>
2024-10-24 18:17:39 +0200

    quest3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _islower - check for lowercase character.
+* @c: The character to check.
+* Return: 1 if "c" is lowercase otherwise 0.
+*/
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
