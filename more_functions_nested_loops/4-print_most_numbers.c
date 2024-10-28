7d1cc100e3f15ee73677796803645cefdeb2c60f
Zeez6 <9864@holbertonstudents.com>
2024-10-28 14:30:48 +0100

    quest4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -7,11 +7,8 @@
 void print_most_numbers(void)
 {
 int number;
-for (number = '0'; number < '9'; number++)
-{
+for (number = '0'; number <= '9'; number++)
 if ((number != '2') || (number != '4'))
-{
 _putchar(number);
-}
 _putchar('\n');
 }
