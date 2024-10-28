3b09fd4a25ac381478caa67068bea9cd9c4dce75
Zeez6 <9864@holbertonstudents.com>
2024-10-28 11:37:31 +0100

    quest3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
 * print_numbers - function that prints the numbers from 0 to 9.
+* followed
 * Return: 0
 */
 void print_numbers(void)
 {
-char c = '0';
-while (c <= '9')
+char c;
+while (c = '9'; c <= '9'; c++);
 {
 _putchar(c);
 c++;
