4dc2ab40eae794e4faa9850ea7d1b188961cfa7d
Zeez6 <9864@holbertonstudents.com>
2024-10-28 10:39:19 +0100

    quest0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _isalpha - check for lowercase or uppercase character.
+* @c: The character to check.
+* Return: 1 if "c" is lowercase or uppercase otherwise 0.
+*/
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
