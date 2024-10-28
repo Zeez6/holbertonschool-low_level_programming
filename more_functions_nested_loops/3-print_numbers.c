0510279235b51375144d2d62e9913b58628c0f3e
Zeez6 <9864@holbertonstudents.com>
2024-10-28 11:34:09 +0100

    quest3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers - function that prints the numbers from 0 to 9.
+* Return: 0
+*/
+void print_numbers(void)
+{
+char c = '0';
+while (c <= '9')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
