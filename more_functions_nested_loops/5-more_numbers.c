e32a612f6ad7bdc831316e6320b5e502e61ec19d
Zeez6 <9864@holbertonstudents.com>
2024-10-28 15:21:28 +0100

    quest5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,13 +2,21 @@
 /**
 * more_numbers - prints 10 times the numbers from 0 to 14.
 * followed by new line.
-* Return : always 0.
+* Return : 0.
 **/
 int more_numbers(void);
 {
-int number;
-for (number = '0'; number <= "14"; number++)
-putchar(number++);
-putchar(number);
-putchar('\n');
+int number1; number2;
+for (number1 = 0; number1 <= 10; number++)
+
+for (number2 = 0; number2 <= 14; number++)
+{
+if (number2 > 9)
+{
+_putchar((number2 / 10) + '0');
 }
+_putchar((number2 % 10) + '0');
+}
+_putchar(10);
+}
+
