02dbaeaeaaf8fd4252c990c7e5b78ccb9ea8074d
Zeez6 <9864@holbertonstudents.com>
2024-10-29 17:27:07 +0100

    quest9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,27 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 100; num++)
-	printf("%d", num);
+	for (num = 1; num <= 100; num++)
+	printf("%d, num");
 	{
-	if (num % 3 == 0)
+	if (num % 3 == 0 && num % 5 == 0)
+	{
+	printf("FizzBuzz");
+	}
+	else if (num % 3 == 0)
+	{
 	printf("Fizz");
 	}
-	if (num % 5 == 0)
-	printf("Buzz");
+	else if (num % 5 == 0)
 	{
-		if (num % 5 == 0 && num % 3 == 0)
-		printf("FizzBuzz");
+		printf("Buzz");
 	}
+	if (num < 100)
+	{
+		printf(" ");
+	}
+	else
 	printf("%d", num);
+	}
+	return (0);
 }
