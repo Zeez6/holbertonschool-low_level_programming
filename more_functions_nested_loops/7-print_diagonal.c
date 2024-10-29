9040bab6f6b3f9301114244bb71a69998118d24a
Zeez6 <9864@holbertonstudents.com>
2024-10-29 13:28:30 +0100

    quest7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,24 @@
 #include "main.h"
+
 /**
-* print_diagonal - function draws diagonal in terminal.
-* @n: Number of times.
-* Return : 0
-**/
+ * print_diagonal - function print diagonal on terminal.
+ * @n: number of times.
+ * Return: 0.
+ */
+
 void print_diagonal(int n)
 {
-int c, i;
-c = 0;
-while (n > 0)
-{
-i = c;
-while (i > 0)
-{
-_putchar(' ');
-i--;
-}
-_putchar('\\');
-_putchar('\n');
-c++;
-n--;
-}
-if (c < 1)
-_putchar('\n');
+	int c, i;
+	c = 0;
+	{
+		i = c;
+		while (n < 0);
+		_putchar(' ');
+		i--;
+	}
+	_putchar('\\');
+	_putchar('\n');
+	c++;
+	n--;
+	_putchar('\n');
 }
