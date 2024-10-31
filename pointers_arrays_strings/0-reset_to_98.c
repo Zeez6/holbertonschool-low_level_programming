2f156f1d89ebf4882f809777b40dcad9f9ebe3fc
Zeez6 <9864@holbertonstudents.com>
2024-10-31 12:39:17 +0100

    gisele0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Main - main function.
+ * Function take pointer.
+ * Return: 0.
+ */
+
+void reset_to_98(int *n)
+
+{
+	int *gisele = n;
+	*gisele = 98;
+}
