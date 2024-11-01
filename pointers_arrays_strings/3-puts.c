4d3a9e62627ec890e4bc0bb25cbf4bc733b39809
Zeez6 <9864@holbertonstudents.com>
2024-11-01 09:40:38 +0100

    quest3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,15 +2,16 @@
 
 /**
  * _puts - Print string.
- * @str: The string to print.
+ * @str: pointer the string to print.
  */
 
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(*str);
-		str++;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
