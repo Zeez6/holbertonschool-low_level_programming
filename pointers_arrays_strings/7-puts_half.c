035c04c4f671b45f24ea49c49c89daa972c64f5c
Zeez6 <9864@holbertonstudents.com>
2024-11-02 17:40:01 +0100

    quest7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *  puts_half - function print half of string.
+ * @str: The input string.
+ */
+
+void puts_half(char *str)
+{
+	int i, a;
+
+	i = 0;
+	{
+		while (str[i] != '\0')
+		i++;
+	}
+	a = i / 2;
+	{
+		if (i % 2 == 1)
+		a++;
+	}
+
+		while ( a < i)
+		{
+			_putchar(str[a]);
+			a++;
+		}
+		_putchar('\n');
+}
