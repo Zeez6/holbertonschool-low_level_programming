49e4ab1fe343d5abcc4e8243ba03bfd5f9a01cac
Zeez6 <9864@holbertonstudents.com>
2024-11-02 17:08:53 +0100

    quest5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,29 +1,28 @@
 #include "main.h"
 
 /**
- * rev_string - function that reverse a string.
- * @s: Function to reverse a string.
+ * rev_string - function that reverses a string.
+ * @s: The string to reverse.
  * Return: nothing.
  */
 
 void rev_string(char *s)
 {
-	int i, j;
-	char gisele[500];
+	int i = 0, length = 0;
+	char temp;
 
-	j = 0;
-
-	while (s[i] != '\0')
+	while (s[length] != '\0')
 	{
+		length++;
+	}
+	length--;
+
+	while (i < length)
+	{
+		temp = s[i];
+		s[i] = s[length];
+		s[length] = temp;
 		i++;
-	}
-	for (i = 0; i < j; i++)
-	{
-		gisele[i] = s[i - 1 - i];
-	}
-	gisele[i] = '\0';
-	for (i = 0; i < j; i++)
-	{
-		s[i] = gisele[i];
+		length--;
 	}
 }
