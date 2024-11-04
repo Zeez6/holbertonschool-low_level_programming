32b3002783156f69903d41720a9e7c4eec229e44
Zeez6 <9864@holbertonstudents.com>
2024-11-04 15:00:37 +0100

    quest0

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert string to integer.
+ * 
+ */
--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -15,3 +15,22 @@ int main(void)
 	printf("%s\n", s);
 	return (0);
 }
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char str[] = "Look up!\n";
+	char *ptr;
+
+	ptr = string_toupper(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
+}
