c9d44be2b600a22e583ab5576bf42996053df8b2
Zeez6 <9864@holbertonstudents.com>
2024-11-04 23:34:22 +0100

    questx

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - capitalize all words of sting.
+ * @str: string modified.
+ * Return: pointer modified string.
+ */
+
+char *cap_string(char *str)
+{
+
+}
+return (str);
+}
