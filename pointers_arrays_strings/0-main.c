e530ad33f0f91b34490349159233022abe264f39
Zeez6 <9864@holbertonstudents.com>
2024-11-05 09:26:19 +0100

    quest0

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -5,7 +5,6 @@
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
- *
  * Return: Nothing.
  */
 void simple_print_buffer(char *buffer, unsigned int size)
@@ -27,4 +26,20 @@ void simple_print_buffer(char *buffer, unsigned int size)
 				i++;
 		}
 		printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char buffer[98] = {0x00};
+
+	simple_print_buffer(buffer, 98);
+	_memset(buffer, 0x01, 95);
+	printf("-------------------------------------------------\n");
+	simple_print_buffer(buffer, 98);    
+	return (0);
 }
