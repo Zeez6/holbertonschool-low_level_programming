5398991197637c77f833607bbb524dde27ff4aeb
Zeez6 <9864@holbertonstudents.com>
2024-11-06 14:56:59 +0100

    quest5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - function that locates a substring.
+ * @needle: substring.
+ * @haystack: string.
+ * @h: pointer.
+ * @n: pointer.
+ * Return: NULL.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *h, *n;
+
+	if (*needle == '\0')
+	return (haystack);
+
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+		return (haystack);
+
+		haystack++;
+	}
+
+	return ('\0');
+}
