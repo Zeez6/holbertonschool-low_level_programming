1e1198dc5536d3a3aca22bf269bec56fac9a3c93
Zeez6 <9864@holbertonstudents.com>
2024-11-08 18:06:37 +0100

    quest1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,19 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * _memcpy - function that copies memory area.
- * @n: bytes.
- * @src:source.
- * @dest: destination.
- * Return: pointer dest.
+ * _memcpy - Copies memory area.
+ * main - main function.
+ * @dest: Destination.
+ * @src: Source.
+ * @n: Number.
+ * Return: Pointer destination.
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-		for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
