03840d58de655994f9730e68483aeeac3231d334
Zeez6 <9864@holbertonstudents.com>
2024-11-08 14:47:19 +0100

    quest0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function.
+ * @argc: number.
+ * @argv: array.
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+    {
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
