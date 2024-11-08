e62a1cd343a954b1b6a411b9cc549bf8b49fa388
Zeez6 <9864@holbertonstudents.com>
2024-11-08 16:41:23 +0100

    quest3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,7 +9,6 @@
  */
 
 int main(int argc, char *argv[])
-
 {
 unsigned int num1;
 unsigned int num2;
@@ -18,10 +17,9 @@ unsigned int result;
 	if (argc != 3)
 	{
 		printf("error\n");
-	}
 		return (1);
+	}
 
-		else
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
 		result = num1 * num2;
