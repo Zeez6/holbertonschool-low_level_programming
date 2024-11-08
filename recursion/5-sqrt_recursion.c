72b4ddcfd8801fc3276aad5f15109a2548f47eed
Zeez6 <9864@holbertonstudents.com>
2024-11-08 13:45:14 +0100

    quest5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,27 +1,33 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - function return the natural square root of numb.
- * sqrt_helper - recursive function that does the real computational work.
- * @n: number calculate square root.
- * @i: estimate.
- * Return: natural square.
+ * _sqrt_helper - recurses to find the natural square root of a number.
+ * @n: number to calculate the square root of.
+ * @i: iterator.
+ *
+ * Return: the resulting square root.
+ */
+
+int _sqrt_helper(int n, int i)
+{
+	if (i * i == n)
+	return (i);
+	if (i * i > n)
+	return (-1);
+	return (_sqrt_helper(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - function that return the natural square root of int.
+ * @n: int value.
+ * Return: factorial given number; if n < 0 return -1.
  */
 
 int _sqrt_recursion(int n)
 {
-	return sqrt_helper(n, 1);
-}
-
-int sqrt_helper(int n, int i)
-{
-	if (i * i == n)
-	{
-	return i;
-	}
-	if (i * i > n)
-	{
-		return -1;
-	}
-	return sqrt_helper(n, i + 1);
+	if (n < 0)
+	return (-1);
+	if (n == 0 || n == 1)
+	return (n);
+	return (_sqrt_helper(n, 1));
 }
