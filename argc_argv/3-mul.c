16fa24910bb7d2a40694c1f53d653faea931e5d6
Zeez6 <9864@holbertonstudents.com>
2024-11-08 16:32:11 +0100

    quest3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,18 +14,16 @@ int main(int argc, char *argv[])
 int num1;
 int num2;
 int result;
-
 {
 	if (argc != 3)
 	{
 		printf("error\n");
 	}
 		return (1);
-	{
+
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
 		result = num1 * num2;
-	}
 	{
 		printf("%d\n", result);
 	}
