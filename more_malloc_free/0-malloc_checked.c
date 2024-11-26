fa616adff29e4ed498a771fb38784cb41cc70791
Zeez6 <9864@holbertonstudents.com>
2024-11-26 10:59:38 +0100

    quest0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - function that allocates memory
+ * @b: int for memory
+ * Return: a pointer to the allocated memory
+ * If fails, function should cause normal termination with a status of 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
